152e3c9a8ea2d245a54264808337bc66d3aed561
rvxx007 <akashkawle0@gmail.com>
2025-03-16 15:16:31 +0530

    final patch updates

--- immverseai/src/components/UserProfile.jsx
+++ immverseai/src/components/UserProfile.jsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import {useRouter} from 'next/navigation';
 
 const UserProfile = ({ obj }) => {
   const auth = getAuth();
