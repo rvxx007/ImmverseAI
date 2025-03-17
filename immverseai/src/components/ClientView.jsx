25f4f1495c34b3cf6e8b415fa6b73c98a37af117
rvxx007 <akashkawle0@gmail.com>
2025-03-17 14:48:00 +0530

    testimonial client view , horizontal scroller is completed

--- immverseai/src/components/ClientView.jsx
+++ immverseai/src/components/ClientView.jsx
@@ -10,12 +10,14 @@ const ClientView = () => {
     <section className='client-view-section'>
         <h1>{appData.testimonial.headTxt}</h1>
         <p className='cv-subhead'>{appData.testimonial.subTxt}</p>
-        <div className='cv-card-div-row'>
+        
+
+        <div className='cv-scroller'>
         {appData.testimonial.tmUser.map((item, index)=>
-            <div className='cv-card' key={index}>
+            <div className='bor cv-scroller-elements' key={index}>
                 <p><span className='cv-span-title'>{item.title}</span><span className='cv-span-ext'>{item.ext}</span></p>
                 <p className='cv-span-msg'>{item.subTxt}</p>
-                <div className='cv-profile-block'>
+                <div className=' cv-profile-block'>
                     <Image width={40} height={40} src={item.img} alt='card view image' />
                     <div className='cd-info-block'>
                         <span className='cv-info-name'>{item.name}</span>
@@ -24,8 +26,6 @@ const ClientView = () => {
                 </div>
             </div>)}
         </div>
-
-        
     </section>
   )
 }
